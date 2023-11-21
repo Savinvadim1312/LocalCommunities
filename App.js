e1642db63532e0133b3575e2e4747ff350d38ccf
Vadim Savin <savinvadim1312@gmail.com>
2023-11-21 15:32:15 +0000

    changed title

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <Text>Local App Communities</Text>
       <StatusBar style="auto" />
     </View>
   );
